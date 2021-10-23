8bfae74cec6f1e083b6438bf29c02ef453e067bd
Chu <c210259@ccg.ac.jp>
2021-10-23 22:08:02 +0900

    fix bug

--- homework/Zodiac.js
+++ homework/Zodiac.js
@@ -8,62 +8,62 @@ function getObject(){
     //document.getElementById("test").innerHTML = DM ; test
       if ( DM >= 321 && DM <=420) {
       document.getElementById("Zodiac").innerHTML = "Aries";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Aries.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Aries.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 421 && DM <=520)  {
       document.getElementById("Zodiac").innerHTML = "Taurus";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Taurus.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Taurus.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 521 && DM <=620)  {
       document.getElementById("Zodiac").innerHTML = "Gemini";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Gemini.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Gemini.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 621 && DM <=722)  {
       document.getElementById("Zodiac").innerHTML = "Cancer";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Cancer.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Cancer.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 723 && DM <=822) {
       document.getElementById("Zodiac").innerHTML = "Leo";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Leo.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Leo.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 823 && DM <=922) {
       document.getElementById("Zodiac").innerHTML = "Virgo";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Virgo.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Virgo.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 923 && DM <=1022) {
       document.getElementById("Zodiac").innerHTML = "Libra";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Libra.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Libra.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 1023 && DM <=1122) {
       document.getElementById("Zodiac").innerHTML = "Scorpio";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Scorpio.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Scorpio.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 1123 && DM <=1221) {
       document.getElementById("Zodiac").innerHTML = "Sagittarius";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Sagittarius.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Sagittarius.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 1222 || DM <=119) {
       document.getElementById("Zodiac").innerHTML = "Capricorn";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Capricorn.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Capricorn.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 120 && DM <=219) {
       document.getElementById("Zodiac").innerHTML = "Aquarius";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Aquarius.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Aquarius.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 		else if ( DM >= 220 && DM <=320) {
       document.getElementById("Zodiac").innerHTML = "Pisces";
-      document.getElementById("img1").innerHTML = '<img id="imgs" src="/img/Zodiac/Pisces.jpg" alt="">' ;
+      document.getElementById("img1").innerHTML = '<img id="imgs" src="img/Zodiac/Pisces.jpg" alt="">' ;
       document.getElementById("test").style.animationName ="Bgrcolor";
       } 
 }
